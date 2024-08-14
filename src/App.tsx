e366ed8b1a0eed1146bb91f4b01d695c5f0639b7
Daniel Ceballos <danielceballos94@gmail.com>
2024-08-13 20:36:44 -0400

    Add initial setup, dependencies

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+import { Container, Typography, Box } from "@mui/material";
+
+function App() {
+  return (
+    <Container maxWidth="lg">
+      <Box sx={{ padding: "20px 0" }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          YouTube Clone
+        </Typography>
+        {/* Add your components here */}
+      </Box>
+    </Container>
+  );
+}
+
+export default App;
